daf41a2e687ac202b3354bb38c41e571370ffe84
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-09-02 21:09:19 +0700

    problem 3. Longest Substring Without Repeating Characters

---
+++ src/main/java/com/learn/slidingwindow/problem3/solution2/MainApp.java
@@ -0,0 +1,33 @@
+package com.learn.slidingwindow.problem3.solution2;
+
+public class MainApp {
+    public static int lengthOfLongestSubstring(String s) {
+        return 0;
+
+    }
+
+    /*
+    Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+    Example 2:
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+    Example 3:
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+    */
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
