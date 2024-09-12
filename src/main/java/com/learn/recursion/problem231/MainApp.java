7c33a2fe4ca61c5226262087dd9cb06ca5fef22a
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-09-12 22:56:12 +0700

    dev problem231

---
+++ src/main/java/com/learn/recursion/problem231/MainApp.java
@@ -0,0 +1,16 @@
+package com.learn.recursion.problem231;
+
+//Title: 231. Power of Two
+//Link: https://leetcode.com/problems/power-of-two/description/?envType=problem-list-v2&envId=recursion&difficulty=EASY
+public class MainApp {
+    public static boolean isPowerOfTwo(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(5));
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(3));
+    }
+}
