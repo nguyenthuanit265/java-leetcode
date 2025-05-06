415585ba5ae9a0d68a7c549da43d714509e98460
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2025-05-06 19:42:31 +0700

    bigo

--- src/main/java/com/learn/grind75/problem1/solution1/MainApp.java
+++ src/main/java/com/learn/grind75/problem1/solution1/MainApp.java
@@ -1,6 +1,6 @@
 package com.learn.grind75.problem1.solution1;
 
-import java.util.Arrays;
+import java.util.*;
 
 //Title: 1. Two Sum
 //Link: https://leetcode.com/problems/two-sum/description/
@@ -22,6 +22,19 @@ public class MainApp {
 
         return new int[]{-1, -1};
     }
+
+    public int[] twoSumV2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            if (map.containsKey(remain)) {
+                return new int[]{map.get(remain), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1}; // Not found
+    }
+
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
