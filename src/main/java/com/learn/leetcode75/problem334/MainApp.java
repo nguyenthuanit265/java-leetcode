643afe494ea8abc129944cbcc08ce08a32c103e0
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2025-08-28 23:27:17 +0700

    code

---
+++ src/main/java/com/learn/leetcode75/problem334/MainApp.java
@@ -0,0 +1,20 @@
+package com.learn.leetcode75.problem334;
+
+public class MainApp {
+    public boolean increasingTriplet(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
