5d8edc1c272653e82b733e3798fe6d84a77b7b0b
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2025-08-30 13:13:49 +0700

    code

--- src/main/java/com/learn/leetcode75/problem334/MainApp.java
+++ src/main/java/com/learn/leetcode75/problem334/MainApp.java
@@ -4,33 +4,17 @@ import java.util.*;
 
 public class MainApp {
     public boolean increasingTriplet(int[] nums) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] > nums[i]) {
-                    if (map.containsKey(j)) {
-                        map.put(i, new ArrayList<>(List.of(j)));
-                    } else {
-                        List<Integer> val = map.get(j);
-                        val.add(j);
-                        map.put(i, val);
-                    }
-                }
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
             }
         }
-        for (List<Integer> values : map.values()) {
-            for (int i = 0; i < values.size(); i++) {
-                for (int j = i + 1; j < values.size(); j++) {
-                    if (values.get(j) > values.get(i)) {
-                       return true;
-                    }
-                }
-            }
-        }
-
-
-
-
 
         return false;
     }
