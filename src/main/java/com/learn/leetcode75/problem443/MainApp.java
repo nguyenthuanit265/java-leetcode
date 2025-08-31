4da0e181ad9faa74aa7ff0a1c889b63e83ba6507
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2025-08-31 18:45:36 +0700

    code

---
+++ src/main/java/com/learn/leetcode75/problem443/MainApp.java
@@ -0,0 +1,29 @@
+package com.learn.leetcode75.problem443;
+
+public class MainApp {
+
+    public int compress(char[] chars) {
+        int read = 0, write = 0;
+        int n = chars.length;
+        while (read < n) {
+            char currentChar = chars[read];
+            int start = read;
+            while (read < n && chars[read] == currentChar) {
+                read++;
+            }
+
+            chars[write++] = currentChar;
+            int count = read - start;
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
