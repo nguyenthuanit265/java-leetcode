c52a16fa9065b9b4ff4acc4eff5421836cdab1cd
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2025-08-31 20:02:57 +0700

    code

--- src/main/java/com/learn/leetcode75/problem643/MainApp.java
+++ src/main/java/com/learn/leetcode75/problem643/MainApp.java
@@ -2,27 +2,15 @@ package com.learn.leetcode75.problem643;
 
 public class MainApp {
     public static double findMaxAverage(int[] nums, int k) {
-        int left = 0, right = 0;
-        double res = 0, temp = 0, sum = 0;
-        while (right < nums.length) {
-            int count = 1;
-            while (count <= k && right < nums.length) {
-                sum += nums[right++];
-                count++;
+        double res = -Double.MAX_VALUE, sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            if (right - left + 1 == k) {
+                double avg = sum / k;
+                res = Math.max(res, avg);
+                sum -= nums[left++];
             }
-
-            if (count < k) {
-                break;
-            }
-
-            temp = sum / k;
-            if (res < temp) {
-                res = temp;
-            }
-
-            sum = 0;
-            left++;
-            right = left;
         }
 
         return res;
