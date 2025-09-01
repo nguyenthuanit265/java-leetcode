3f3bb62333720c5dc465e927ebbdfc3524a977f6
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2025-09-01 14:37:26 +0700

    code

--- src/main/java/com/learn/leetcode75/problem11/MainApp.java
+++ src/main/java/com/learn/leetcode75/problem11/MainApp.java
@@ -21,6 +21,31 @@ public class MainApp {
         return max;
     }
 
+
+    public static int maxArea2(int[] height) {
+        int max = Integer.MIN_VALUE;
+        int n = height.length;
+        int left = 0, right = n - 1;
+
+        if (height.length == 0 || height.length == 1) {
+            return 0;
+        }
+
+        max = (right - left) * Math.min(height[left], height[right]);
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            max = Math.max(max, width * minHeight);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
